61534247eb67a4fa6774557631c1aee5a35abf48
lbferrand <lbferrand@gmail.com>
2022-08-19 19:23:27 -0400

    Update Navbar.jsx

--- modern_ui_ux_react/src/components/Navbar.jsx
+++ modern_ui_ux_react/src/components/Navbar.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { useState } from "react";
+
+import { close, logo, menu } from "../assets";
+import { navLinks } from "../constants";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    /*w-full, flex py-6 are different styles from tailwind */
+    <nav className="w-full flex py-6 justify-between items-center navbar">
+      <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
+
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+        {navLinks.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={`font-poppins font-normal cursor-pointer text-[16px] ${
+              index === navLinks.length - 1 ? "mr-0" : "mr-10"
+            } text-white`}
+          >
+            <a href={`#${nav.id}`}>{nav.title}</a>
+          </li>
+        ))}
+      </ul>
+
+      {/* new list for mobile devise menu */}
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;

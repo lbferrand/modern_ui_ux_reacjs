2f97ca65e8ce6a563450fa3e72abd0ee0fc2b6d1
lbferrand <lbferrand@gmail.com>
2022-08-19 20:01:31 -0400

    Finished Navbar component

--- modern_ui_ux_react/src/components/Navbar.jsx
+++ modern_ui_ux_react/src/components/Navbar.jsx
@@ -5,6 +5,8 @@ import { close, logo, menu } from "../assets";
 import { navLinks } from "../constants";
 
 const Navbar = () => {
+  // toggle to open and close the menu in small devicess
+  const [toggle, setToggle] = useState(false);
   return (
     /*w-full, flex py-6 are different styles from tailwind */
     <nav className="w-full flex py-6 justify-between items-center navbar">
@@ -25,7 +27,30 @@ const Navbar = () => {
 
       {/* new list for mobile devise menu */}
       <div className="sm:hidden flex flex-1 justify-end items-center">
-        <img />
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h=[28px] object-contain"
+          onClick={() => setToggle((prev) => !prev)}
+        />
+        <div
+          className={`${
+            toggle ? "flex" : "hidden"
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        >
+          <ul className="list-none flex flex-col justify-end items-center flex-1">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-normal cursor-pointer text-[16px] ${
+                  index === navLinks.length - 1 ? "mr-0" : "mb-4"
+                } text-white`}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </nav>
   );
